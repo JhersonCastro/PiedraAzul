291c188a13d4a1d9fbee0d3da84c4469ee47190f
JhersonCastro <jhersoncastro@unicauca.edu.co>
2026-03-22 17:19:45 -0500

    Add get doctor by id async

--- PiedraAzul/PiedraAzul/ApplicationServices/Services/DoctorService.cs
+++ PiedraAzul/PiedraAzul/ApplicationServices/Services/DoctorService.cs
@@ -8,9 +8,15 @@ namespace PiedraAzul.ApplicationServices.Services
     public interface IDoctorService
     {
         Task<List<DoctorProfile>> GetDoctorByTypeAsync(DoctorType type);
+        Task<DoctorProfile> GetDoctorByIdAsync(Guid doctorId);
     }
     public class DoctorService(IDbContextFactory<AppDbContext> dbContextFactory) : IDoctorService
     {
+        public Task<DoctorProfile> GetDoctorByIdAsync(Guid doctorId)
+        {
+            throw new NotImplementedException();
+        }
+
         public async Task<List<DoctorProfile>> GetDoctorByTypeAsync(DoctorType type)
         {
             using var context = await dbContextFactory.CreateDbContextAsync();
